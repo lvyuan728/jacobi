bd2f7d02f8e214a9659b5ca688aceddad06efcd5
lvyuan728 <lv.yuan.728@gmail.com>
2026-07-12 14:10:45 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,8 +1,21 @@
+A
+print(theta_block([1,1,2,3,4,4,5,5,6,7],-6,5))
+B
+print(theta_block([1,1,2,2,3,3,4,4,5,7,8],-6,5))
+C
+print(theta_block([1,2,2,3,3,4,4,5,7,7],-6,5))
+
+Ex 7.3 
+
 print(theta_block([1,1,1,2,2,2,3,3,4,5],0,4))
+
+Ex7.4
+print(theta_block([1,1,1,2,2,3,3,4,4,5],-6,4))
 
 g= JacobiForms(43).cusp_forms_basis(2,5)[0]
 g.gritsenko_lift()
 
+Ex 7.5
 g=JacobiForms(53).cusp_forms_basis(2,5)[0]
 g.gritsenko_lift()
 
